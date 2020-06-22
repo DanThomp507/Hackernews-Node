93b832dc95ee5cd591139685157c4e90eddd605d
Daniel Thompson <DanThomp507@gmail.com>
2020-06-21 21:46:54 -0400

    more unit testing practice

--- src/spec.js
+++ src/spec.js
@@ -3,3 +3,17 @@ describe('My Test Suite', () => {
     expect(true).toEqual(true);
   });
 });
+
+function callMyFunction(callback) {
+  callback();
+}
+
+describe('callMyFunction function', () => {
+  it('calls the passed function', () => {
+    const callback = jest.fn();
+
+    callMyFunction(callback);
+
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+});
